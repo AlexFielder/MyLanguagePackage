f1f3f17800f16b224fa937d1f0a0e2e510a103e2
Alex Fielder <alex.fielder@gmail.com>
2017-02-10 12:30:43 +0000

    Initial Commit

---
+++ MyLanguagePackage/MyLanguageService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Package;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+
+namespace MyLanguagePackage
+{
+    class MyLanguageService : LanguageService
+    {
+        public override string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string GetFormatFilterList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override LanguagePreferences GetLanguagePreferences()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IScanner GetScanner(IVsTextLines buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override AuthoringScope ParseSource(ParseRequest req)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
